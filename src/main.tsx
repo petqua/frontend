da8dac38831964df195ff07f5a9cf1dd4e110685
blueapple99 <blueapple99@naver.com>
2024-01-24 06:40:13 +0900

    chore: MSW 설치 및 설정
    - 핸들러 작성
    - Service Worker 작성

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+const { DEV } = import.meta.env;
+import { worker } from './mocks/browser.ts';
+
+if (DEV) {
+  worker.start();
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
