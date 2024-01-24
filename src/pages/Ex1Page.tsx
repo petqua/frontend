e4740c9b43c80b582394fce01d3113950043f558
blueapple99 <blueapple99@naver.com>
2024-01-25 03:57:30 +0900

    chore: zustand 설정
    - zustand 설치 및 간단한 예제 적용

--- src/pages/Ex1Page.tsx
+++ src/pages/Ex1Page.tsx
@@ -1,5 +1,14 @@
+import useStore from '../states/Ex1';
+
 const Ex1Page = () => {
-  return <div>1아울렛입니당</div>;
+  const { count, increment, decrement } = useStore();
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={increment}>증가</button>
+      <button onClick={decrement}>감소</button>
+    </div>
+  );
 };
 
 export default Ex1Page;
