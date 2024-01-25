f74512008215f1c422768258f327bf3e19b44028
seungzzok <ki2684@naver.com>
2024-01-25 18:52:36 +0900

    feat: Text atom 제작

---
+++ src/components/atoms/Text.tsx
@@ -0,0 +1,44 @@
+import { DefaultTheme, styled } from 'styled-components';
+
+interface TextProps {
+  children: React.ReactNode; // 글자 텍스트
+  size: number; // 글자 사이즈
+  color: string; // 글자 색상
+  style?: any; // 이외 스타일
+}
+
+interface FontProps {
+  theme: DefaultTheme;
+  fontTheme: string;
+}
+
+export const RegularText = ({ children, size, color, style }: TextProps) => {
+  const fontTheme = `regular${size}`;
+  return (
+    <Text fontTheme={fontTheme} style={{ ...style, color: color }}>
+      {children}
+    </Text>
+  );
+};
+
+export const MediumText = ({ children, size, color, style }: TextProps) => {
+  const fontTheme = `medium${size}`;
+  return (
+    <Text fontTheme={fontTheme} style={{ ...style, color: color }}>
+      {children}
+    </Text>
+  );
+};
+
+export const BoldText = ({ children, size, color, style }: TextProps) => {
+  const fontTheme = `bold${size}`;
+  return (
+    <Text fontTheme={fontTheme} style={{ ...style, color: color }}>
+      {children}
+    </Text>
+  );
+};
+
+const Text = styled.div<FontProps>`
+  ${({ theme, fontTheme }) => theme.font[fontTheme]}
+`;
