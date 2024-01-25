cc9c03acdf79cf22a182ae2d1e97329ede73c936
seungzzok <ki2684@naver.com>
2024-01-25 20:06:25 +0900

    feat: ProductImg atom 제작

---
+++ src/components/atoms/ProductImg.tsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+interface ProductImg {
+  size: string;
+  src: string;
+}
+
+const ProductImg = ({ size, src }: ProductImg) => {
+  return (
+    <ImgContainer style={{ width: size, height: size }}>
+      <Image src={src || ''} />
+    </ImgContainer>
+  );
+};
+
+export default ProductImg;
+
+const ImgContainer = styled.div`
+  background-color: ${({ theme }) => theme.color.gray[30]};
+  border-radius: 12px;
+  overflow: hidden;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
