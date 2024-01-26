72886374c4781a0917968265def67517d4c80e84
blueapple99 <blueapple99@naver.com>
2024-01-27 08:50:52 +0900

    feat: 홈페이지 추가
    - 홈페이지 추가

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,6 @@
 import Ex1Page from './Ex1Page';
 import Ex2Page from './Ex2Page';
 import MainPage from './MainPage';
+import HomePage from './HomePage';
 
-export { Ex1Page, Ex2Page, MainPage };
+export { Ex1Page, Ex2Page, MainPage, HomePage };
