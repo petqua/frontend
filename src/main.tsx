2f7766a7bdd6f9cfa53ea867c923c8c449676dd6
blueapple99 <blueapple99@naver.com>
2024-01-29 04:10:45 +0900

    chore: MSW 사용 잠정적 보류
    - proxy 설정과 겹쳐서 잘 안됨
    - 생각만큼 편리하지 않음

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+
+/*
 const { DEV } = import.meta.env;
 import { worker } from './mocks/browser.ts';
-
 if (DEV) {
   worker.start();
 }
+*/
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,21 +1,37 @@
 import { HttpResponse, http } from 'msw';
 
 const handlers = [
-  http.get('/api/users', () => {
-    return HttpResponse.json([
-      {
-        id: 'abc-123',
-        name: '김가나',
-      },
-      {
-        id: 'def-456',
-        name: '박다라',
-      },
-      {
-        id: 'ghi-789',
-        name: '이마바',
-      },
-    ]);
+  http.get('/api/announcements', () => {
+    return HttpResponse.json({
+      notification:
+        '[공지] 펫쿠아 앱 출시 기념 이벤트 진행중 ! 안전운송 사전 신청하러 가기',
+    });
+  }),
+  http.get('/api/banners', () => {
+    return HttpResponse.json({
+      data: [
+        {
+          id: '1L',
+          imageUrl: '/images/1.jpg',
+          linkUrl: 'https://www.naver.com/',
+        },
+        {
+          id: '2L',
+          imageUrl: '/images/2.jpg',
+          linkUrl: 'https://www.naver.com/',
+        },
+        {
+          id: '3L',
+          imageUrl: '/images/3.jpg',
+          linkUrl: 'https://www.naver.com/',
+        },
+        {
+          id: '4L',
+          imageUrl: '/images/4.jpg',
+          linkUrl: 'https://www.naver.com/',
+        },
+      ],
+    });
   }),
 ];
 
