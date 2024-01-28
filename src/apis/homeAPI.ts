7d5797a65fe0ed1f31e6a25ba7cc85590219078e
blueapple99 <blueapple99@naver.com>
2024-01-29 04:13:32 +0900

    feat: 배너 이미지 api 연동
    - 캐러셀 컴포넌트 props 수정
    - 배너 이미지 api 추가
    - dotenv 설치

---
+++ src/apis/homeAPI.ts
@@ -0,0 +1,17 @@
+import { client } from './axiosInstance';
+
+export const getBannersAPI = async () => {
+  try {
+    const res = await client.get('/banners');
+    return res.data;
+  } catch (error: any) {
+    if (error.response) {
+      // 서버 응답이 있는 경우 (오류 상태 코드 처리)
+      console.error('Server Error:', error.response.data);
+    } else {
+      // 서버 응답이 없는 경우 (네트워크 오류 등)
+      console.error('Error creating question:', error.message);
+    }
+    throw error;
+  }
+};
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,18 +1,47 @@
+import { useQuery } from '@tanstack/react-query';
 import { Carousel, FullScreen, Notification } from '../components/molecules';
 import { CategoryList } from '../components/organisms';
+import { getBannersAPI } from '../apis';
 
-const CAROUSEL_IMAGES = [
-  '/images/1.jpg',
-  '/images/2.jpg',
-  '/images/3.jpg',
-  '/images/4.jpg',
+// 일단 서버에서 내려주는 값이 없어서 이렇게 해놓음.
+const CAROUSEL_MOCK_DATA = [
+  {
+    id: '1L',
+    imageUrl: '/images/1.jpg',
+    linkUrl: 'https://www.naver.com/',
+  },
+  {
+    id: '2L',
+    imageUrl: '/images/2.jpg',
+    linkUrl: 'https://www.naver.com/',
+  },
+  {
+    id: '3L',
+    imageUrl: '/images/3.jpg',
+    linkUrl: 'https://www.naver.com/',
+  },
+  {
+    id: '4L',
+    imageUrl: '/images/4.jpg',
+    linkUrl: 'https://www.naver.com/',
+  },
 ];
 
 const HomePage = () => {
+  const { data: bannersList } = useQuery({
+    queryKey: ['banners'],
+    queryFn: getBannersAPI,
+    staleTime: 60 * 1000,
+  });
+
   return (
     <FullScreen>
       <Notification />
-      <Carousel carouselList={CAROUSEL_IMAGES} />
+      {bannersList && bannersList.length !== 0 ? (
+        <Carousel carouselList={bannersList} />
+      ) : (
+        <Carousel carouselList={CAROUSEL_MOCK_DATA} />
+      )}
       <Notification />
       <CategoryList />
     </FullScreen>
