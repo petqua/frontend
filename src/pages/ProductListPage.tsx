a02948f71202d9579a7a4ee7f4610929a7c10837
seungzzok <ki2684@naver.com>
2024-01-30 19:23:20 +0900

    feat: 상품 리스트 레이아웃 구현

--- src/pages/ProductListPage.tsx
+++ src/pages/ProductListPage.tsx
@@ -1,28 +1,101 @@
 import { useState } from 'react';
+import { FlexBox } from '../components/atoms';
 import { Modal, Filter } from '../components/molecules';
+import { ProductList } from '../components/organisms';
 
 const ProductListPage = () => {
-  const options = [
+  const sortOptions = [
     { name: 'SALE_PRICE_ASC', title: '낮은 가격순' },
     { name: 'SALE_PRICE_DESC', title: '높은 가격순' },
     { name: 'REVIEW_COUNT_DESC', title: '리뷰 많은 순' },
   ];
-  const [value, setValue] = useState('필터');
-  const [isOpenModal, setIsOpenModal] = useState(false);
+  const [sortValue, setSortValue] = useState('필터');
+  const [isOpenSortModal, setIsOpenSortModal] = useState(false);
+
+  const list = [
+    {
+      imgUrl: '',
+      title: '구피 50마리',
+      price: '30,000',
+      discountRate: '30',
+      discountedPrice: '21,000',
+      like: 23,
+      review: 1,
+    },
+    {
+      imgUrl: '',
+      title: '구피 50마리',
+      price: '30,000',
+      discountRate: '30',
+      discountedPrice: '21,000',
+      like: 23,
+      review: 1,
+    },
+    {
+      imgUrl: '',
+      title: '구피 50마리',
+      price: '30,000',
+      discountRate: '30',
+      discountedPrice: '21,000',
+      like: 23,
+      review: 1,
+    },
+    {
+      imgUrl: '',
+      title: '구피 50마리',
+      price: '30,000',
+      discountRate: '30',
+      discountedPrice: '21,000',
+      like: 23,
+      review: 1,
+    },
+    {
+      imgUrl: '',
+      title: '구피 50마리',
+      price: '30,000',
+      discountRate: '30',
+      discountedPrice: '21,000',
+      like: 23,
+      review: 1,
+    },
+    {
+      imgUrl: '',
+      title: '구피 50마리',
+      price: '30,000',
+      discountRate: '30',
+      discountedPrice: '21,000',
+      like: 23,
+      review: 1,
+    },
+    {
+      imgUrl: '',
+      title: '구피 50마리',
+      price: '30,000',
+      discountRate: '30',
+      discountedPrice: '21,000',
+      like: 23,
+      review: 1,
+    },
+  ];
 
   return (
-    <>
-      <Filter value={value} setIsOpenModal={setIsOpenModal} />
-      {isOpenModal && (
+    <FlexBox col gap="2.4rem" style={{ padding: '1.4rem' }}>
+      <FlexBox gap="1.2rem" align="center">
+        <Filter value={sortValue} setIsOpenModal={setIsOpenSortModal} />
+      </FlexBox>
+      <ProductList listData={list} length={list.length} />
+
+      {/* =================== 모달 =================== */}
+      {isOpenSortModal && (
         <Modal
-          options={options}
+          options={sortOptions}
           title="필터"
-          value={value}
-          setIsOpenModal={setIsOpenModal}
-          setValue={setValue}
+          value={sortValue}
+          setIsOpenModal={setIsOpenSortModal}
+          setValue={setSortValue}
         />
       )}
-    </>
+    </FlexBox>
   );
 };
 
