67debb9406d135c1c2bd715445f5a8290b797423
seungzzok <ki2684@naver.com>
2024-01-31 13:28:20 +0900

    fix: 머지오류 해결

--- src/components/organisms/RecommendList.tsx
+++ src/components/organisms/RecommendList.tsx
@@ -1,86 +1,37 @@
+import { useQuery } from '@tanstack/react-query';
 import { FlexBox } from '../atoms';
 import {
   ProductListItem,
   PreviewListTitle,
   RowScrollContainer,
 } from '../molecules';
+import { getProductsAPI } from '../../apis/productAPI';
 
 const RecommendList = () => {
-  const RCM_LIST = [
-    {
-      imgUrl: '',
-      title: '구피 50마리',
-      price: '30,000',
-      discountRate: '30',
-      discountedPrice: '21,000',
-      like: 23,
-      review: 1,
-    },
-    {
-      imgUrl: '',
-      title: '구피 50마리',
-      price: '30,000',
-      discountRate: '30',
-      discountedPrice: '21,000',
-      like: 23,
-      review: 1,
-    },
-    {
-      imgUrl: '',
-      title: '구피 50마리',
-      price: '30,000',
-      discountRate: '30',
-      discountedPrice: '21,000',
-      like: 23,
-      review: 1,
-    },
-    {
-      imgUrl: '',
-      title: '구피 50마리',
-      price: '30,000',
-      discountRate: '30',
-      discountedPrice: '21,000',
-      like: 23,
-      review: 1,
-    },
-    {
-      imgUrl: '',
-      title: '구피 50마리',
-      price: '30,000',
-      discountRate: '30',
-      discountedPrice: '21,000',
-      like: 23,
-      review: 1,
-    },
-    {
-      imgUrl: '',
-      title: '구피 50마리',
-      price: '30,000',
-      discountRate: '30',
-      discountedPrice: '21,000',
-      like: 23,
-      review: 1,
-    },
-    {
-      imgUrl: '',
-      title: '구피 50마리',
-      price: '30,000',
-      discountRate: '30',
-      discountedPrice: '21,000',
-      like: 23,
-      review: 1,
-    },
-  ];
+  const { data: productListData } = useQuery({
+    queryKey: ['products', { limit: 7, sourceType: 'HOME_RECOMMENDED' }],
+    queryFn: getProductsAPI,
+    staleTime: 60 * 1000,
+  });
+
+  // 타입 예외처리
+  const products = productListData?.products || [];
 
   return (
     <FlexBox col gap="2.4rem" style={{ padding: '2.4rem 0' }}>
       <PreviewListTitle title="펫쿠아 강력 추천!" path="/recommend" />
-      <ProductListItem isMain data={RCM_LIST[0]} />
-      <RowScrollContainer gap="1.2rem" row={1} col={6}>
-        {RCM_LIST.filter((item, idx) => idx > 0).map((item, idx) => (
-          <ProductListItem key={idx} isSmall data={item} />
-        ))}
-      </RowScrollContainer>
+      <ProductListItem isMain data={products[0]} />
+      {products.length > 1 ? (
+        <RowScrollContainer gap="1.2rem" row={1} col={6}>
+          {products
+            ?.filter((item, idx) => idx > 0)
+            ?.map((item, idx) => (
+              <ProductListItem key={idx} isSmall data={item} />
+            ))}
+        </RowScrollContainer>
+      ) : (
+        <div>Data Error</div>
+      )}
     </FlexBox>
   );
 };
