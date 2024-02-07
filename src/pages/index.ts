2ce7b6feea743ced2220bf1cc91e434482ae459d
seungzzok <ki2684@naver.com>
2024-02-07 15:55:33 +0900

    chore: WishListPage url 설정

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -3,5 +3,15 @@ import MainPage from './MainPage';
 import HomePage from './HomePage';
 import ProductListPage from './ProductListPage';
 import SearchPage from './SearchPage';
+import WishListPage from './WishListPage';
+// import ProductDetailPage from './ProductDetailPage';
 
-export { Ex1Page, MainPage, HomePage, ProductListPage, SearchPage };
+export {
+  Ex1Page,
+  MainPage,
+  HomePage,
+  ProductListPage,
+  SearchPage,
+  WishListPage,
+  // ProductDetailPage,
+};
