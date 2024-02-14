89d00bccd606e72936fc57e4278eaba248887789
blueapple99 <blueapple99@naver.com>
2024-02-14 23:49:32 +0900

    feat: axios interceptors 추가
    - 요청 보낼 때 token 있으면 header에 추가
    - 에러 응답 받을 때 에러 코드에 따라 에러 핸들링
    - A01이면 accessToken 갱신
    - 나머지 A코드는 다시 로그인 요청

--- src/apis/axiosInstance.ts
+++ src/apis/axiosInstance.ts
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import { useAuthStore } from '../states';
+import { getAccessTokenAPI } from '.';
 //const DEV = import.meta.env;
 
 //const baseURL = DEV ? '/api' : 'https://api.petqua.co.kr';
@@ -9,3 +11,52 @@ export const client = axios.create({
   timeout: 5000,
   withCredentials: true,
 });
+
+client.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('accessToken');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+client.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const { setAccessToken, logout } = useAuthStore();
+    const customStatusCode = error.response.data.code;
+    switch (customStatusCode) {
+      case 'A01': {
+        const res = await getAccessTokenAPI();
+        const newAccessToken = res.headers['authorization'];
+        setAccessToken(newAccessToken);
+        console.error('로그인 유지 API 적용', customStatusCode);
+        break;
+      }
+      case 'A02':
+      case 'A10':
+      case 'A11':
+      case 'A12':
+      case 'A13':
+      case 'A20':
+      case 'A30':
+        logout();
+        window.location.href = '/login';
+        console.error('소셜 로그인 유도', customStatusCode);
+        break;
+      case 'P10':
+        console.error('유효하지 않은 검색어', customStatusCode);
+        break;
+      default:
+        console.error('알 수 없는 상태 코드: ', customStatusCode);
+    }
+    return Promise.reject(error);
+  },
+);
