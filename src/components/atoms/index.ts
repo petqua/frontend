ada2bb2af718ee65ad82b6b27f0f1bd92a5e750f
seungzzok <ki2684@naver.com>
2024-02-20 20:54:35 +0900

    feat: profileImg atom 제작

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,6 +1,7 @@
 import FlexBox from './FlexBox';
 import KeywordText from './KeywordText';
 import ProductImg from './ProductImg';
+import ProfileImg from './ProfileImg';
 import { LightText, RegularText, MediumText, BoldText } from './Text';
 
 export {
@@ -11,4 +12,5 @@ export {
   MediumText,
   BoldText,
   KeywordText,
+  ProfileImg,
 };
