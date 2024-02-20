1b495e64fafe110d4e635eb40f6be3a4f643bb16
seungzzok <ki2684@naver.com>
2024-02-20 21:25:49 +0900

    feat: 리뷰 조회 api 연동

--- src/apis/reviewAPI.ts
+++ src/apis/reviewAPI.ts
@@ -1,5 +1,38 @@
 import { client } from './axiosInstance';
-import { GetReviewStatisticsAPI } from '../interfaces/review';
+import {
+  GetReviewsAPIParams,
+  GetReviewsAPI,
+  GetReviewStatisticsAPI,
+} from '../interfaces/review';
+
+export const getReviewsAPI = async ({
+  productId,
+  lastViewedId,
+  limit,
+  sorter,
+  score,
+  photoOnly,
+}: GetReviewsAPIParams): Promise<GetReviewsAPI> => {
+  try {
+    const res = await client.get(`/products/${productId}/reviews`, {
+      params: {
+        lastViewedId,
+        limit,
+        sorter,
+        score,
+        photoOnly,
+      },
+    });
+    return res.data;
+  } catch (error: any) {
+    if (error.response) {
+      console.error('Server Error:', error.response.data);
+    } else {
+      console.error('Error creating question:', error.message);
+    }
+    throw error;
+  }
+};
 
 export const getReviewStatisticsAPI = async (
   productId: number,
--- src/interfaces/review.ts
+++ src/interfaces/review.ts
@@ -1,8 +1,39 @@
+export interface GetReviewsAPIParams {
+  productId: number;
+  lastViewedId?: number;
+  limit: number;
+  sorter?: string;
+  score?: number;
+  photoOnly?: boolean;
+}
+
+export interface GetReviewsAPI {
+  productReviews: ReviewItemData[];
+  hasNextPage: boolean;
+}
+
 export interface GetReviewStatisticsAPI {
   scoreCounts: number[];
   productSatisfaction: number;
   totalReviewCount: number;
   averageScore: number;
+}
+
+export interface ReviewItemData {
+  id: number;
+  productId: number;
+  score: number;
+  content: string;
+  recommendCount: number;
+  hasPhotos: true;
+  createdAt: string;
+  reviewerId: number;
+  reviewerName: string;
+  reviewerProfileImageUrl: string;
+  reviewerFishBowlCount: number;
+  reviewerYears: number;
+  recommended: boolean;
+  images: string[];
 }
 
 export interface ReviewItem {
