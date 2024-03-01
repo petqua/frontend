cced2718de8d28a797ef03139ed38b4f05c8a4a0
seungzzok <ki2684@naver.com>
2024-03-01 15:38:12 +0900

    feat: 봉달목록 레이아웃 디자인 변경
    - 판매처 별로 상품 분류
    - 운송비 토글 기능
    - 상품 별 운송비 계산

--- src/components/molecules/CartItem.tsx
+++ src/components/molecules/CartItem.tsx
@@ -6,94 +6,132 @@ import { OptionModal } from '../organisms';
 import Confirm from './Confirm';
 import { LiaWindowClose } from 'react-icons/lia';
 import { FaCheck } from 'react-icons/fa6';
+import { CartItem } from '../../interfaces/cart';
 
-const CartItem = ({ data }: { data: any }) => {
+const CartItem = ({ data, handleSelectItem }: CartItem) => {
   const [isOpenModal, setIsOpenModal] = useState(false);
   const [isOpenConfirm, setIsOpenConfirm] = useState(false);
-  const [checked, setChecked] = useState(true);
+
+  const getMethod = (method: string) => {
+    if (method === 'COMMON') return '일반운송';
+    if (method === 'SAFETY') return '안전운송';
+    if (method === 'PICKUP') return '직접픽업';
+    return 'error';
+  };
+
+  const getSex = (sex: string) => {
+    if (sex === 'MALE') return '수컷';
+    if (sex === 'FEMALE') return '암컷';
+    if (sex === 'PICKUP') return '자웅동체';
+    return 'error';
+  };
 
   return (
     <>
-      <FlexBox padding="1.2rem 0" gap="0.6rem" fullWidth>
-        <CheckBox checked={checked}>
+      <Container>
+        <CheckBox checked={data?.checked}>
           <input
             type="checkbox"
-            checked={checked}
-            onChange={(e) => setChecked(e.currentTarget.checked)}
+            checked={data?.checked}
+            onChange={(e) =>
+              handleSelectItem(
+                data?.storeName,
+                data?.id,
+                e.currentTarget.checked,
+              )
+            }
           />
           <div>
             <FaCheck
               size={12}
               color={theme.color.tint.white}
               style={{
-                visibility: checked ? 'visible' : 'hidden',
+                visibility: data?.checked ? 'visible' : 'hidden',
               }}
             />
           </div>
         </CheckBox>
-        <FlexBox gap="1.2rem" align="center" style={{ flex: 1 }}>
-          <ProductImg size="9rem" src={data?.productThumbnailUrl} />
-          <FlexBox
-            col
-            justify="space-between"
-            style={{ flex: 1, minHeight: '9rem' }}
-          >
-            <FlexBox justify="space-between" fullWidth>
-              <FlexBox col gap="0.4rem" style={{ flex: 1 }}>
-                <RegularText size={12} color={theme.color.gray[50]}>
-                  {data?.storeName}
-                </RegularText>
-                <MediumText
-                  size={16}
-                  color={theme.color.gray.main}
-                  style={{ lineHeight: '120%' }}
-                >
-                  {data?.productName}
-                </MediumText>
-              </FlexBox>
-              <LiaWindowClose
-                size={18}
-                color={theme.color.gray[50]}
-                style={{ cursor: 'pointer' }}
-                onClick={() => setIsOpenConfirm(true)}
-              />
-            </FlexBox>
-
-            <FlexBox col gap="0.8rem" fullWidth>
-              <FlexBox justify="space-between" align="center" fullWidth>
-                <RegularText size={12} color={theme.color.gray[50]}>
-                  마릿수 {data?.quantity} | {data?.isMale ? '수컷' : '암컷'}
-                </RegularText>
-                {data?.productDiscountRate && (
-                  <FlexBox align="center" gap="0.4rem">
-                    <MediumText size={12} color={theme.color.tint.red}>
-                      [{data?.productDiscountRate}%]
-                    </MediumText>
-                    <RegularText
-                      size={12}
-                      color={theme.color.gray[50]}
-                      style={{ textDecoration: 'line-through' }}
-                    >
-                      {data?.productPrice.toLocaleString()} 원
-                    </RegularText>
-                  </FlexBox>
-                )}
+        <FlexBox col gap="1.6rem" style={{ flex: 1 }}>
+          <FlexBox gap="1rem" align="center" fullWidth>
+            <ProductImg size="9.6rem" src={''} />
+            <FlexBox
+              col
+              justify="space-between"
+              style={{ flex: 1, minHeight: '9rem' }}
+            >
+              <FlexBox justify="space-between" fullWidth>
+                <FlexBox col gap="0.4rem" style={{ flex: 1 }}>
+                  <MediumText
+                    size={16}
+                    color={theme.color.gray.main}
+                    style={{ lineHeight: '120%' }}
+                  >
+                    {data?.productName}
+                    <MethodTag $method={data?.deliveryMethod}>
+                      {getMethod(data?.deliveryMethod)}
+                    </MethodTag>
+                  </MediumText>
+                </FlexBox>
+                <LiaWindowClose
+                  size={18}
+                  color={theme.color.gray[50]}
+                  style={{ cursor: 'pointer' }}
+                  onClick={() => setIsOpenConfirm(true)}
+                />
               </FlexBox>
 
-              <FlexBox justify="space-between" align="center" fullWidth>
-                <OptionButton onClick={() => setIsOpenModal(true)}>
+              <FlexBox col gap="0.8rem" fullWidth>
+                <FlexBox justify="space-between" align="center" fullWidth>
                   <RegularText size={12} color={theme.color.gray[50]}>
-                    옵션변경
+                    {data?.quantity}마리 | {getSex(data?.sex)}
                   </RegularText>
-                </OptionButton>
-                <MediumText size={18} color={theme.color.gray.main}>
-                  {data?.productDiscountPrice.toLocaleString()} 원
-                </MediumText>
+                  {data?.productDiscountRate && (
+                    <FlexBox align="center" gap="0.4rem">
+                      <MediumText size={12} color={theme.color.tint.red}>
+                        [{data?.productDiscountRate}%]
+                      </MediumText>
+                      <RegularText
+                        size={12}
+                        color={theme.color.gray[50]}
+                        style={{ textDecoration: 'line-through' }}
+                      >
+                        {data?.productPrice.toLocaleString()} 원
+                      </RegularText>
+                    </FlexBox>
+                  )}
+                </FlexBox>
+
+                <FlexBox justify="space-between" align="center" fullWidth>
+                  <OptionButton onClick={() => setIsOpenModal(true)}>
+                    <RegularText size={12} color={theme.color.gray[50]}>
+                      옵션변경
+                    </RegularText>
+                  </OptionButton>
+                  <MediumText size={14} color={theme.color.gray.main}>
+                    {data?.productDiscountPrice.toLocaleString()} 원
+                  </MediumText>
+                </FlexBox>
               </FlexBox>
             </FlexBox>
           </FlexBox>
+          {data?.deliveryMethod !== 'PICKUP' && (
+            <PriceCalculatorBox>
+              <MediumText
+                size={12}
+                color={theme.color.blue[70]}
+                style={{ textAlign: 'center' }}
+              >
+                {`상품금액 ${data?.productDiscountPrice.toLocaleString()} + 
+              ${getMethod(data?.deliveryMethod)}비 
+              ${data?.deliveryFee.toLocaleString()} = 
+              ${(
+                data?.productDiscountPrice + data?.deliveryFee
+              ).toLocaleString()}`}
+              </MediumText>
+            </PriceCalculatorBox>
+          )}
         </FlexBox>
-      </FlexBox>
+      </Container>
       {isOpenModal && <OptionModal setIsOpenModal={setIsOpenModal} />}
       {isOpenConfirm && (
         <Confirm
@@ -107,6 +145,16 @@ const CartItem = ({ data }: { data: any }) => {
 };
 
 export default CartItem;
+
+const Container = styled.section`
+  display: flex;
+  align-items: flex-start;
+  padding: 1rem 0.8rem;
+  gap: 0.6rem;
+  width: 100%;
+  background-color: white;
+  border-radius: 1.2rem;
+`;
 
 const OptionButton = styled.button`
   padding: 0.4rem 0.6rem;
@@ -144,4 +192,31 @@ const CheckBox = styled.label<{ checked: boolean }>`
     background: ${({ checked, theme }) =>
       checked ? theme.color.blue[80] : 'white'};
   }
+`;
+
+const MethodTag = styled.div<{ $method: string }>`
+  padding: 0.3rem 0.6rem;
+  border-radius: 0.6rem;
+  background-color: ${({ $method, theme }) =>
+    $method === 'COMMON'
+      ? theme.color.gray[40]
+      : $method === 'SAFETY'
+        ? theme.color.blue[80]
+        : theme.color.gray.main};
+  color: ${({ $method, theme }) =>
+    $method === 'COMMON' ? theme.color.gray.main : theme.color.tint.white};
+  font-size: 1rem;
+  font-weight: 700;
+  display: inline-block;
+  vertical-align: middle;
+  margin-left: 0.4rem;
+  line-height: 1;
+  margin-bottom: 0.2rem;
+`;
+
+const PriceCalculatorBox = styled.div`
+  width: 100%;
+  border-radius: 3rem;
+  border: 0.05rem solid ${({ theme }) => theme.color.blue[70]};
+  padding: 0.6rem;
 `;
--- src/interfaces/payment.ts
+++ src/interfaces/payment.ts
@@ -7,18 +7,3 @@ export interface Address {
   detailAddress: string;
   isDefaultAddress: boolean;
 }
-
-export interface CartItemDetails {
-  id: number;
-  storeName: string;
-  productId: number;
-  productName: string;
-  productThumbnailUrl: string;
-  productPrice: number;
-  productDiscountRate: number;
-  productDiscountPrice: number;
-  quantity: number;
-  isMale: boolean;
-  deliveryMethod: string;
-  isOnSale: boolean;
-}
