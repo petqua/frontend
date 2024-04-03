72b1cc8fd1932e7cb5427eb7c2eba015a2a259f5
blueapple99 <blueapple99@naver.com>
2024-04-03 10:08:54 +0900

    refactor : 배송 요청 사항 드랍다운 수정
    - 배송 요청 사항 전역에서 관리하게 끔 변경

--- src/states/PaymentStore.ts
+++ src/states/PaymentStore.ts
@@ -14,11 +14,20 @@ interface Address {
 interface StoreState {
   address: Address | null;
   setAddress: (inputValue: Address) => void;
+  shippingRequest: string;
+  setShippingRequest: (option: string) => void;
+  customShippingRequest: string;
+  setCustomShippingRequest: (input: string) => void;
 }
 
 const usePaymentStore = create<StoreState>((set) => ({
   address: null,
   setAddress: (inputAddress: Address) => set({ address: inputAddress }),
+  shippingRequest: '배송시 요청사항을 선택해주세요',
+  setShippingRequest: (option: string) => set({ shippingRequest: option }),
+  customShippingRequest: '',
+  setCustomShippingRequest: (input: string) =>
+    set({ customShippingRequest: input }),
 }));
 
 export default usePaymentStore;
